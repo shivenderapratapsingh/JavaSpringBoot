8cc054ce772b89b78e3832f7b1570e461fa04173
shivenderapratapsingh <shivendrapartapsingh365@gmail.com>
2026-01-02 23:50:15 +0530

    RestApiFinish

--- RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/ToDoController.java
+++ RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/ToDoController.java
@@ -1,13 +1,13 @@
 package com.example.RestApiTodoApp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class ToDoController {
     public static ArrayList<Response>  todos;
     public  ToDoController(){
@@ -15,16 +15,50 @@ public class ToDoController {
         todos.add(new Response(12,false,"Goat",123));
         todos.add(new Response(23,true,"Messi",987));
     }
-    @GetMapping("/todos")
-    public ArrayList<Response> getTodos(){
-        return  todos;
+    @GetMapping()
+    public ResponseEntity<ArrayList<Response>> getTodos(){
+        return  ResponseEntity.ok(todos);
     }
-    @PostMapping("/todos")
-    public Response CreateTodo(@RequestBody Response newTodo){
+
+
+    @PostMapping()
+//    @ResponseStatus(HttpStatus.CREATED) we use this annotation to set status
+    public ResponseEntity<Response> CreateTodo(@RequestBody Response newTodo){
         todos.add(newTodo);
-        return newTodo;
-
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
-
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<Response> getTodoById(@PathVariable Long todoId){
+        for(Response todo: todos){
+            if(todo.getId()==todoId){
+                return ResponseEntity.ok(todo);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+    // In this Code i dont use request mapping which api versoning when we do this we don't need to write any it it like postfix
+//    @GetMapping("/todos")
+//    public ResponseEntity<ArrayList<Response>> getTodos(){
+//        return  ResponseEntity.ok(todos);
+//    }
+//
+//
+//    @PostMapping("/todos")
+////    @ResponseStatus(HttpStatus.CREATED) we use this annotation to set status
+//    public ResponseEntity<Response> CreateTodo(@RequestBody Response newTodo){
+//        todos.add(newTodo);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+//    }
+//
+//    @GetMapping("/todos/{todoId}")
+//    public ResponseEntity<Response> getTodoById(@PathVariable Long todoId){
+//        for(Response todo: todos){
+//            if(todo.getId()==todoId){
+//                return ResponseEntity.ok(todo);
+//            }
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
 }
+
