bd4cce126b233646208cd646209755ddb3e7a679
shivenderapratapsingh <shivendrapartapsingh365@gmail.com>
2026-01-02 22:26:53 +0530

    start

--- RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/ToDoController.java
+++ RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/ToDoController.java
@@ -1,5 +1,8 @@
 package com.example.RestApiTodoApp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,4 +15,16 @@ public class ToDoController {
         todos.add(new Response(12,false,"Goat",123));
         todos.add(new Response(23,true,"Messi",987));
     }
+    @GetMapping("/todos")
+    public ArrayList<Response> getTodos(){
+        return  todos;
+    }
+    @PostMapping("/todos")
+    public Response CreateTodo(@RequestBody Response newTodo){
+        todos.add(newTodo);
+        return newTodo;
+
+    }
+
+
 }
