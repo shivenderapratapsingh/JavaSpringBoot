5ef71c97dd77579dd409eaccfe3e950bc6d5d020
shivenderapratapsingh <shivendrapartapsingh365@gmail.com>
2026-01-03 22:26:13 +0530

    Delete or Update

--- RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/Response.java
+++ RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/Response.java
@@ -1,22 +1,22 @@
 package com.example.RestApiTodoApp;
 
 public class Response{
-	private int id;
+	private Long id;
 	private boolean completed;
 	private String title;
 	private int userId;
 
-    public Response(int id,boolean completed,String title,int userId){
+    public Response(Long id,boolean completed,String title,int userId){
         this.id=id;
         this.completed=completed;
         this.title=title;
         this.userId=userId;
     }
-	public void setId(int id){
+	public void setId(Long id){
 		this.id = id;
 	}
 
-	public int getId(){
+	public Long getId(){
 		return id;
 	}
 
--- RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/ToDoController.java
+++ RestApiTodoApp/src/main/java/com/example/RestApiTodoApp/ToDoController.java
@@ -12,8 +12,8 @@ public class ToDoController {
     public static ArrayList<Response>  todos;
     public  ToDoController(){
         todos=new ArrayList<>();
-        todos.add(new Response(12,false,"Goat",123));
-        todos.add(new Response(23,true,"Messi",987));
+        todos.add(new Response(12L,false,"Goat",123));
+        todos.add(new Response(23L,true,"Messi",987));
     }
     @GetMapping()
     public ResponseEntity<ArrayList<Response>> getTodos(){
@@ -29,14 +29,43 @@ public class ToDoController {
     }
 
     @GetMapping("/todos/{todoId}")
-    public ResponseEntity<Response> getTodoById(@PathVariable Long todoId){
+    public ResponseEntity<?> getTodoById(@PathVariable Long todoId){
         for(Response todo: todos){
             if(todo.getId()==todoId){
                 return ResponseEntity.ok(todo);
             }
         }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("todo with id"+todoId+"not Found");
     }
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<?> deleteById(@PathVariable Long todoId) {
+
+        boolean removed = todos.removeIf(t -> t.getId().equals(todoId));
+
+      if(removed){
+          return ResponseEntity.ok("Todo deletes successfully");
+      }
+      else{
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Data related to that Id not found");
+      }
+    }
+
+    @PutMapping("/{todoId}")
+    public ResponseEntity<?> UpdateById(@PathVariable Long todoId,@RequestBody Response UpdateTodo){
+        for(Response todo:todos){
+            if(todo.getId().equals(todoId)){
+                todo.setTitle(UpdateTodo.getTitle());
+                todo.setUserId(UpdateTodo.getUserId());
+                todo.setCompleted(UpdateTodo.isCompleted());
+                return ResponseEntity.ok(todo);
+            }
+
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Data related to that id not Exsist in the List");
+    }
+
+
+
     // In this Code i dont use request mapping which api versoning when we do this we don't need to write any it it like postfix
 //    @GetMapping("/todos")
 //    public ResponseEntity<ArrayList<Response>> getTodos(){
