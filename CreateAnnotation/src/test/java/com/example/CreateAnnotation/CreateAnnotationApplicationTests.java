5bf3dfd6e50e0f41e1875a218db6418aa77a5a94
shivenderapratapsingh <shivendrapartapsingh365@gmail.com>
2026-01-06 14:58:42 +0530

    Anootation

---
+++ CreateAnnotation/src/test/java/com/example/CreateAnnotation/CreateAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreateAnnotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
